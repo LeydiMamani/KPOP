770cc091c9c9858549021879abc02434f7dae309
LeydiMamani <leydi.mamani.chivigorry@gmail.com>
2024-09-26 12:03:36 -0500

    vecinos cercanos

--- expo.py
+++ expo.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 file_path = 'C:\\LM\\IA\\kpop ia\\kpop_rankings.csv' 
 df = pd.read_csv(file_path) 
@@ -15,24 +16,25 @@ df = pd.read_csv(file_path)
 
 # Funcion para recomendar las canciones mejor rankeadas de un artista
 def recomendar_canciones_por_artista(artista):
-    # Filtra las canciones del artista
-    canciones_artista = df[df['artist'].str.contains(artista, case=False)]  # Ignorar mayusculas/minusculas
+    # Filtrar canciones del artista
+    canciones_artista = df[df['artist'].str.contains(artista, case=False)]
 
-    # Si el artista no tiene canciones en el dataset
+    # Si el artista no está en el dataset
     if canciones_artista.empty:
         print(f"No se encontraron canciones para el artista: {artista}")
         return
 
-    # Ordenar por ranking y mostrar las 5 mejores
-    mejores_canciones = canciones_artista.sort_values(by='rank').head(600)
+    # Eliminar duplicados basados en 'song_title'
+    canciones_artista_unicas = canciones_artista.drop_duplicates(subset='song_title')
+
+    # Mostrar las 5 mejores
+    mejores_canciones = canciones_artista_unicas.sort_values(by='rank').head(10)
 
     print(f"Las 5 mejores canciones de {artista} son:\n")
-    print(mejores_canciones[['song_title', 'rank']])
+    print(mejores_canciones[['song_title', 'rank', 'artist']])
 
-# Preguntar al usuario por un artista
-artista_usuario = input("Ingresa el nombre de un artista o grupo que te guste: ")
+# Pedir al usuario que seleccione un artista
+artista_usuario = input("Ingresa el nombre de un artista que te guste: ")
 
-# Llamar a la funcion de recomendaciones
+# Llamado a la función de recomendación
 recomendar_canciones_por_artista(artista_usuario)
-
-
